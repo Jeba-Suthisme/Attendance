d4ebeb26ada3ca72f28d1563c9631b3410dbc1ea
Jeba Suthisme <jebasuthisme@gmail.com>
2025-12-26 11:59:15 +0530

    updated

--- BACKEND/myproject/loginapp/views.py
+++ BACKEND/myproject/loginapp/views.py
@@ -8,13 +8,11 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import authentication_classes
 from rest_framework.authentication import TokenAuthentication
-
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.http import HttpResponse
 from datetime import date
 import csv
-
 from attendance.models import Attendance
 from attendance.serializer import AttendanceSerializer
 from .permission import IsInternUserRole
